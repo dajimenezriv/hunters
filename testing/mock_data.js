521a65b3ac3e1d6983018bf1773b781a7fe3ad61
Daniel Jiménez Rivera <djimenezriv@gmail.com>
2022-09-02 00:35:01 +0200

    automatized testing posts

--- testing/mock_data.js
+++ testing/mock_data.js
@@ -2,7 +2,7 @@ import createChats from './mock_chats';
 import createPosts from './mock_posts';
 
 const createDb = async (dispatch) => {
-  await createPosts(dispatch);
+  await createPosts(dispatch, 200);
   await createChats(dispatch);
 }
 
--- testing/mock_posts.js
+++ testing/mock_posts.js
@@ -3,126 +3,82 @@ import * as usersService from 'services/users';
 import * as postsService from 'services/posts';
 import * as postsReducer from 'reducers/posts';
 
-const stagUrl = 'https://firebasestorage.googleapis.com/v0/b/hunters-66bec.appspot.com/o/stag.png?alt=media&token=23177268-9207-4b09-b5b4-9aa39c2a3b84'; // eslint-disable-line
-const stag2Url = 'https://firebasestorage.googleapis.com/v0/b/hunters-66bec.appspot.com/o/stag2.jpg?alt=media&token=db7148e9-a842-4900-ad4e-dbca5db8e4a7'; // eslint-disable-line
 const dajimenezrivId = 'PUgoiV6Mb1POIZPAhRFkcitqdsQ2';
 const martaId = 'Etwhd7KsaDR3ZFdBe924Vnt3DqB3';
 const javierId = 'UNIg9zbqb9VBTjXB1oIMjrfKUCi1';
 
-const createPosts = async (dispatch) => {
+const userIds = [dajimenezrivId, martaId, javierId];
+
+const imagesUri = [
+  null, null,
+  'https://firebasestorage.googleapis.com/v0/b/hunters-66bec.appspot.com/o/stag.png?alt=media&token=23177268-9207-4b09-b5b4-9aa39c2a3b84',
+  'https://firebasestorage.googleapis.com/v0/b/hunters-66bec.appspot.com/o/stag2.jpg?alt=media&token=db7148e9-a842-4900-ad4e-dbca5db8e4a7',
+]
+
+const initialRegion = {
+  latitude: 39.939998,
+  longitude: -3.8168354,
+  latitudeDelta: 8.5,
+  longitudeDelta: 8.5,
+};
+
+const rand = (max) => Math.floor(Math.random() * max);
+
+const getRandomUser = (users) => users[rand(users.length)];
+const getRandomImageUri = () => imagesUri[rand(imagesUri.length)];
+
+const getRandomLocation = () => {
+  const { latitude, longitude, latitudeDelta, longitudeDelta } = initialRegion;
+  return {
+    latitude: latitude + (Math.random() - 0.5) * latitudeDelta,
+    longitude: longitude + (Math.random() - 0.5) * longitudeDelta
+  }
+}
+
+const getRandomLikes = () => {
+  const likes = [];
+  Array(rand(userIds.length)).fill().forEach(() => {
+    const userId = userIds[rand(userIds.length)];
+    if (!likes.includes(userId)) likes.push(userId);
+  })
+  return likes;
+};
+
+const createPosts = async (dispatch, num) => {
   const dajimenezriv = await usersService.getById(dajimenezrivId);
   const marta = await usersService.getById(martaId);
   const javier = await usersService.getById(javierId);
 
+  const users = [
+    { userId: dajimenezrivId, username: dajimenezriv.username, avatarUri: dajimenezriv.avatarUri },
+    { userId: martaId, username: marta.username, avatarUri: marta.avatarUri },
+    { userId: javierId, username: javier.username, avatarUri: javier.avatarUri },
+  ];
+
   await postsService.deleteAll();
 
-  await postsService.add({
-    user: {
-      userId: dajimenezrivId,
-      username: dajimenezriv.username,
-      avatarUri: dajimenezriv.avatarUri,
-    },
-    description: 'Something.',
-    imageUri: null,
-    createdAt: new Date(),
-    likes: [javierId],
-    comments: [],
+  const promise = new Promise((resolve, reject) => {
+    let counter = num;
+    Array(num).fill().map(async () => {
+      getRandomLikes();
+      await postsService.add({
+        user: getRandomUser(users),
+        location: getRandomLocation(),
+        description: 'Something.',
+        imageUri: getRandomImageUri(),
+        createdAt: new Date(),
+        likes: getRandomLikes(),
+        comments: [],
+      });
+
+      counter -= 1;
+      if (counter === 0) resolve();
+    })
   });
 
-  await postsService.add({
-    user: {
-      userId: martaId,
-      username: marta.username,
-      avatarUri: marta.avatarUri,
-    },
-    description: 'Second.',
-    imageUri: stagUrl,
-    createdAt: new Date(),
-    likes: [martaId],
-    comments: [],
-  });
-
-  await postsService.add({
-    user: {
-      userId: javierId,
-      username: javier.username,
-      avatarUri: javier.avatarUri,
-    },
-    // eslint-disable-next-line
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam semper tincidunt sapien, quis pulvinar risus faucibus ac. Integer tempus lorem a tellus lobortis pellentesque. Nullam leo massa, consequat ut elementum vel, varius vitae neque. Pellentesque at viverra purus. Aliquam at imperdiet diam. Fusce eu mi pharetra, vehicula eros vel, bibendum nisi. Sed facilisis mauris eget porta fringilla. Donec gravida suscipit quam sed tempus. Etiam eget risus eu lacus feugiat porta. In hac habitasse platea dictumst. Maecenas pellentesque eros vel eros pharetra, non dignissim felis bibendum.',
-    imageUri: null,
-    createdAt: new Date(),
-    likes: [martaId],
-    comments: [],
-  });
-
-  await postsService.add({
-    user: {
-      userId: martaId,
-      username: marta.username,
-      avatarUri: marta.avatarUri,
-    },
-    description: 'Second.',
-    imageUri: stag2Url,
-    createdAt: new Date(),
-    likes: [javierId, dajimenezrivId],
-    comments: [],
-  });
-
-  await postsService.add({
-    user: {
-      userId: martaId,
-      username: marta.username,
-      avatarUri: marta.avatarUri,
-    },
-    description: 'Second.',
-    imageUri: null,
-    createdAt: new Date(),
-    likes: [javierId],
-    comments: [],
-  });
-
-  await postsService.add({
-    user: {
-      userId: martaId,
-      username: marta.username,
-      avatarUri: marta.avatarUri,
-    },
-    description: 'Second.',
-    imageUri: stag2Url,
-    createdAt: new Date(),
-    likes: [dajimenezrivId],
-    comments: [],
-  });
-
-  await postsService.add({
-    user: {
-      userId: dajimenezrivId,
-      username: dajimenezriv.username,
-      avatarUri: dajimenezriv.avatarUri,
-    },
-    // eslint-disable-next-line
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam semper tincidunt sapien, quis pulvinar risus faucibus ac. Integer tempus lorem a tellus lobortis pellentesque. Nullam leo massa, consequat ut elementum vel, varius vitae neque. Pellentesque at viverra purus. Aliquam at imperdiet diam. Fusce eu mi pharetra, vehicula eros vel, bibendum nisi. Sed facilisis mauris eget porta fringilla. Donec gravida suscipit quam sed tempus. Etiam eget risus eu lacus feugiat porta. In hac habitasse platea dictumst. Maecenas pellentesque eros vel eros pharetra, non dignissim felis bibendum.',
-    imageUri: stagUrl,
-    createdAt: new Date(),
-    likes: [martaId, javierId],
-    comments: [],
-  });
-
-  await postsService.add({
-    user: {
-      userId: javierId,
-      username: javier.username,
-      avatarUri: javier.avatarUri,
-    },
-    description: 'Second.',
-    imageUri: null,
-    createdAt: new Date(),
-    likes: [javierId, dajimenezrivId],
-    comments: [],
-  });
+  await promise;
 
   dispatch(postsReducer.getAll());
-}
+};
 
 export default createPosts;
